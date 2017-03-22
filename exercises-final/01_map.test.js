b33e75a0bef7fa3113f093a47c6f5345b4978da3
Garry Taylor <garrytaylor@fluentmoney.co.uk>
2017-03-22 16:53:41 +0000

    Update Exercises Update the exercises to commit the generated code, makes it easier for people who want to pull and play.

--- exercises-final/01_map.test.js
+++ exercises-final/01_map.test.js
@@ -1,4 +1,4 @@
-const noop = () => {}
+const noop = () => { }
 
 test('Import immutable lib', () => {
   var Immutable = require('immutable');
@@ -16,6 +16,56 @@ test('Create a Map', () => {
 test('Get Map property', () => {
   var Immutable = require('immutable');
   var map1 = Immutable.Map({ a: 1, b: 2, c: 3 });
+  var b = map1.get("b");
+  expect(b)
+    .toEqual(2);
+})
+
+test('update a Map', () => {
+  var Immutable = require('immutable');
+  var map1 = Immutable.Map({ a: 1, b: 2, c: 3 });
+  var map2 = map1.set("a", 10);
   expect(map1.toJS())
     .toEqual({ a: 1, b: 2, c: 3 });
+
+  expect(map2.toJS())
+    .toEqual({ a: 10, b: 2, c: 3 });
+})
+
+test('update a Map using merge', () => {
+  var Immutable = require('immutable');
+  var map1 = Immutable.Map({ a: 1, b: 2, c: 3 });
+  var map2 = map1.merge({ "a": 10, "d": 4 });
+  expect(map1.toJS())
+    .toEqual({ a: 1, b: 2, c: 3 });
+
+  expect(map2.toJS())
+    .toEqual({ a: 10, b: 2, c: 3, d: 4 });
+})
+
+test.only('update a Map using update', () => {
+  var Immutable = require('immutable');
+  var map1 = Immutable.Map({ a: 1, b: 2, c: 3 });
+  var map2 = map1
+    .set("b", 3)
+    .update("b", (v) => {
+      return v > 2 ? 99 : v;
+    });
+  expect(map1.toJS())
+    .toEqual({ a: 1, b: 2, c: 3 });
+
+  expect(map2.toJS())
+    .toEqual({ a: 1, b: 99, c: 3 });
+
+})
+
+test('Delete from map', () => {
+  var Immutable = require('immutable');
+  var map1 = Immutable.Map({ a: 1, b: 2, c: 3 });
+  var map2 = map1.delete("a");
+  expect(map1.toJS())
+    .toEqual({ a: 1, b: 2, c: 3 });
+
+  expect(map2.toJS())
+    .toEqual({ b: 2, c: 3 });
 })
--- exercises/01_map.test.js
+++ exercises/01_map.test.js
@@ -1,4 +1,4 @@
-const noop = () => {}
+const noop = () => { }
 
 test('Import immutable lib', () => {
   // Import Immutable
@@ -14,6 +14,44 @@ test('Create a Map', () => {
 
 test('Get Map property', () => {
   // Get a property using get(...)
+  expect(b)
+    .toEqual(2);
+})
+
+test('update a Map', () => {
+  // Get a property using set(...)
   expect(map1.toJS())
     .toEqual({ a: 1, b: 2, c: 3 });
+
+  expect(map2.toJS())
+    .toEqual({ a: 10, b: 2, c: 3 });
+})
+
+test('update a Map using merge', () => {
+  // Update and add properties using merge(...)
+  expect(map1.toJS())
+    .toEqual({ a: 1, b: 2, c: 3 });
+
+  expect(map2.toJS())
+    .toEqual({ a: 10, b: 2, c: 3, d: 4 });
+})
+
+test.only('update a Map using update', () => {
+  // Update property "B" to 99, if value is greater than 2 using update(...)
+  // hint, you can chain Immutable methods together : set(...).update(...)
+  expect(map1.toJS())
+    .toEqual({ a: 1, b: 2, c: 3 });
+
+  expect(map2.toJS())
+    .toEqual({ a: 1, b: 99, c: 3 });
+
+})
+
+test('Delete from map', () => {
+  // Delete a property from the map using delete(...)
+  expect(map1.toJS())
+    .toEqual({ a: 1, b: 2, c: 3 });
+
+  expect(map2.toJS())
+    .toEqual({ b: 2, c: 3 });
 })
